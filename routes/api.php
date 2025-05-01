a0132dc0a32f24628bc1b3c4b18d38bf6d066c5f
abdulkarim.salem <abdullarimsalem@gmail.com>
2025-05-01 15:41:00 +0300

    test send email with mailtrap

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmailController;
 use App\Http\Controllers\AuthController;
+
 
 Route::get('/hello',  function (){
     return"hello man";
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/send-email', [EmailController::class, 'sendEmail']);
